9bd2653ce2aba38e1ae81482a52e820c2bc832d4
MustafaPolatov <mustafapolatov18@gmail.com>
2022-09-15 20:44:20 +0500

    Tugadi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,322 @@
 import React, { useEffect, useState } from 'react'
 import "./style.css"
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useNavigate } from 'react-router-dom';
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
-
-export const Home = () => {
-    const [data, setdata] = useState([])
-
-    const axios = require('axios').default;
-
-    useEffect(() => {
-      axios.get("")
-      .then((v)=>{
-        console.log(v);
-      })
-      .catch((err)=>{
-        console.log(err);
-      })
-    }, [])
-    
-
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <div>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export const Home = () => {
+  const [data, setData] = useState(
+    {
+      burger: [],
+      coming: [],
+      iceCream: [],
+      pizza: []
+    }
+  )
+  const [card, setCard] = useState([])
+  const axios = require('axios').default;
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    axios.get("https://myjson.dit.upm.es/api/bins/6qau")
+      .then((v) => {
+        console.log(v.data);
+        setData(v.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [])
+
+
+  function Korzina(v) {
+    let a = [...card]
+    a.push(v)
+    setCard(a)
+  }
+
+  let navigate = useNavigate()
+
+  function GotoPage() {
+    navigate(
+      "/korzinka",
+      {
+        state: card
+      }
+    )
+  }
+
+  function Like(i) {
+
+  }
+
+  console.log(card);
+  return (
+    <div>
+      <header >
+        <div className="menu">
+          <ul>
+            <li className='zag'>TBayEAT</li>
+          </ul>
+          <ul>
+            <li className='orta_li'>Home</li>
+            <li className='orta_li'>About</li>
+            <li className='orta_li'>Menu</li>
+            <li className='orta_li'>Contact</li>
+          </ul>
+          <ul>
+            <li>
+              <Button variant="primary" onClick={handleShow}>
+                Launch
+              </Button>
+
+              <Offcanvas show={show} onHide={handleClose} placement={"end"}>
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title>Korzina</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  {
+                    card.map((v, i) => {
+                      return <div>
+                        <div className="card mb-2">
+                          <img src={v.img} alt="" />
+                          <div className="korzina_card">
+                            <p>{v.name}</p>
+                            <p>{v.price}</p>
+                          </div>
+                        </div>
+                      </div>
+                    })
+                  }
+                  <button onClick={() => GotoPage()} className="btn btn-success mt-2"> Go to Korzina Page</button>
+                </Offcanvas.Body>
+              </Offcanvas>
+            </li>
+          </ul>
+        </div>
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-6">
+              <div>
+                <p className='heder_zag'>Authentic local <br /> food in Tbay</p>
+                <p className='lorem-header'>TBayEAT is a courier service in which authentic home cook food is delived to a customer</p>
+                <div className='rozim'>
+                  <input type="text" className='input_tic' />
+                  <button className='Sell'>Sell</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <img src="cuate.png" alt="" className='rasm_2' />
+            </div>
+          </div>
+        </div>
+      </header>
+      <section>
+        <div className="container">
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                <Tab label="Item One" {...a11yProps(0)} />
+                <Tab label="Item Two" {...a11yProps(1)} />
+                <Tab label="Item Three" {...a11yProps(2)} />
+                <Tab label="Item Three" {...a11yProps(3)} />
+              </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+              <div className="row">
+                {
+                  (data.burger.length > 0)
+                    ? (
+                      data.burger.map((v, i) => {
+                        return <div className="col-4 p-3">
+                          <div className="box">
+                            <img src={v.img} alt="" className='img-fluid rasm_4' />
+                            <div className="chet">
+                              <h4 className='name-v'>{v.name}</h4>
+                              <p className='price_v'>Som {v.price}</p>
+                            </div>
+                            <div className="d-flex">
+                              <button className='button_otvor' onClick={() => Korzina(v)} >Korzinkaga qoshish</button>
+                            </div>
+
+
+                          </div>
+                        </div>
+
+
+                      })
+                    )
+                    : (<h1>Yoqol</h1>)
+                }
+              </div>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <div className="row">
+                {
+                  (data.coming.length > 0)
+                    ? (
+                      data.coming.map((v, i) => {
+                        return <div className="col-4 p-3">
+                          <div className="box">
+                            <img src={v.img} alt="" className='img-fluid rasm_4' />
+                            <div className="chet">
+                              <h4 className='name-v'>{v.name}</h4>
+                              <p className='price_v'>Som {v.price}</p>
+                            </div>
+                          </div>
+                        </div>
+
+
+                      })
+                    )
+                    : (<h1>Yoqol</h1>)
+                }
+              </div>
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              <div className="row">
+                {
+                  (data.iceCream.length > 0)
+                    ? (
+                      data.iceCream.map((v, i) => {
+                        return <div className="col-4 p-3">
+                          <div className="box">
+                            <img src={v.img} alt="" className='img-fluid rasm_4' />
+                            <div className="chet">
+                              <h4 className='name-v'>{v.name}</h4>
+                              <p className='price_v'>Som {v.price}</p>
+                            </div>
+                          </div>
+                        </div>
+
+
+                      })
+                    )
+                    : (<h1>Yoqol</h1>)
+                }
+              </div>
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+              <div className="row">
+                {
+                  (data.pizza.length > 0)
+                    ? (
+                      data.pizza.map((v, i) => {
+                        return <div className="col-4 p-3">
+                          <div className="box">
+                            <img src={v.img} alt="" className='img-fluid rasm_4' />
+                            <div className="chet">
+                              <h4 className='name-v'>{v.name}</h4>
+                              <p className='price_v'>Som {v.price}</p>
+                            </div>
+                          </div>
+                        </div>
+
+
+                      })
+                    )
+                    : (<h1>Yoqol</h1>)
+                }
+              </div>
+            </TabPanel>
+          </Box>
+        </div>
+      </section>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-6 mt-4">
+              <div>
+                <p className='foot_p_1'>
+                  Do you have a question
+                  or want to become a seller?
+                </p>
+                <p>
+                Fill this form and our manager will contact you next 48 hours.
+                </p>
+                <div className="d-flex mb-2">
+                  <input type="text" className='me-1 foot_input' placeholder='Your Name' />
+                  <input type="text"  className='foot_input' placeholder="Your Email"/>
+                </div>
+                <textarea name="" id="" cols="48" rows="5" placeholder='Your message'></textarea>
+              </div>
+            </div>
+            <div className="col-6">
+              <img src="dostavka.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="pas-menu">
+            <div className="menu-2">
+              <ul>
+                <li className='fs-3'>TBayEAT</li>
+              </ul>
+              <ul>
+                <li className='ms-2 '>Home</li>
+                <li className='ms-2 '>About</li>
+                <li className='ms-2 '>Contact</li>
+                <li className='ms-2 '>Login</li>
+              </ul>
+              <ul>
+                <li>
+                  <img src="instagram.png" alt=""  className='insta'/>
+                </li>
+                <li>
+                  <img src="Facebook.png" alt="" />
+                </li>
+              </ul>
+            </div>
+        </div>
+      </footer>
     </div>
   )
 }
--- src/pages/Korzinka.js
+++ src/pages/Korzinka.js
@@ -1,8 +1,214 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import "./style.css"
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from 'react-bootstrap/Modal';
+
+const steps = ['Tanlang', 'Kartangizni Kiriting', 'Sotib Olindi'];
 
 export const Korzinka = () => {
+
+
+  let local = useLocation().state
+
+  const [loc, setLoc] = useState(local)
+
+  console.log(loc);
+
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+
+  const isStepOptional = (step) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>Korzinka</div>
+    <div>
+      <header >
+        <div className="menu">
+          <ul>
+            <li className='zag'>TBayEAT</li>
+          </ul>
+          <ul>
+            <li className='orta_li'>Home</li>
+            <li className='orta_li'>About</li>
+            <li className='orta_li'>Menu</li>
+            <li className='orta_li'>Contact</li>
+          </ul>
+          <ul>
+            <li>
+                <Link to="/" className='Back'>Back</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-6">
+              <div>
+                <p className='heder_zag'>Authentic local <br /> food in Tbay</p>
+                <p className='lorem-header'>TBayEAT is a courier service in which authentic home cook food is delived to a customer</p>
+                <div className='rozim'>
+                  <input type="text" className='input_tic' />
+                  <button className='Sell'>Sell</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <img src="cuate.png" alt="" className='rasm_2' />
+            </div>
+          </div>
+        </div>
+      </header>
+
+      
+      <div className="container">
+      <h2 className='m-3'>Product:</h2>
+      {
+        loc.map((v, i) => {
+          return <div className="row card_korzin">
+            <div className="col-12">
+              <div className="row">
+              
+                <div className="col-4 d-flex">
+                  
+                  <img src={v.img} alt="" className='img-fluid rasm_korzin' />
+                  <div>
+                    <p className='korzina_p'>{v.name}</p>
+                    <p className='korzina_p_2'>beef patties, Iceberg lettuce, American , ...</p>
+                  </div>
+                </div>
+                <div className="col-8">
+                    <div className='d-flex mt-3'>
+                          <p className='korzin_price'> {v.price} Som</p>
+                          <p className='korzin_desc'>{v.desc}</p>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        })
+      }
+      
+    <button variant="primary" className=' btn_modal btn btn-success' onClick={handleShow}>
+       Sotib Olish
+      </button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><Box sx={{ width: '100%' }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps = {};
+          const labelProps = {};
+          if (isStepOptional(index)) {
+            labelProps.optional = (
+              <Typography variant="caption"></Typography>
+            );
+          }
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <React.Fragment>
+          <p>Salom</p>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleReset}>Reset</Button>
+          </Box>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mr: 1 }}
+            >
+              Back
+            </Button>
+            <Box sx={{ flex: '1 1 auto' }} />
+            {isStepOptional(activeStep) && (
+              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                Skip
+              </Button>
+            )}
+
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+            </Button>
+          </Box>
+        </React.Fragment>
+      )}
+    </Box></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </div>
+    </div>
   )
 }
